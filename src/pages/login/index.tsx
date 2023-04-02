d5ba54b80d843ef59f43aeb4ba5fdf8cbd8fc598
s2alua <s2alua.fu@gmail.com>
2023-04-02 11:59:33 +0000

    Style: Add login form with animation

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,26 @@ import './login.css';
 export default () => {
 	return (
 		<div className='container'>
-			login
+			<div className='box'>
+				<form className='form'>
+					<h2>Sign in</h2>
+					<div className='inputBox'>
+						<input type='text' required />
+						<span>Username</span>
+						<i></i>
+					</div>
+					<div className='inputBox'>
+						<input type='password' required />
+						<span>password</span>
+						<i></i>
+					</div>
+					<div className='links'>
+						<a href='javascript:void(0)'>Forgot Password</a>
+						<a href='javascript:void(0)'>sign-up</a>
+					</div>
+					<input type='submit' value="Login"/>
+				</form>
+			</div>
 		</div>
 	)
 }
