a19da71a822ed6f299cfa57090b5738537742c1b
s2alua <s2alua.fu@gmail.com>
2023-04-02 12:33:32 +0000

    Feat: Redirect to login page if not logined yet

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+
 export default () => {
+	const Navigate = useNavigate();
+	useEffect(()=>{
+		if(!sessionStorage.getItem('login')) {
+			Navigate("/login");
+		}
+	},[])
 	return (
 		<>
-		home
+			home
 		</>
 	)
 }
