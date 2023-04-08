956b379988cc9100b1e54f87153a2759e366881b
s2alua <s2alua.fu@gmail.com>
2023-04-08 06:30:32 +0000

    Feat: Add New Register form and showing animation

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import './login.css';
 
 export default () => {
 	const [ email, setEmail ] = useState<string>('');
 	const [ password, setPassword ] = useState<string>('');
+	const [ isLoginPage, setIsLoginPage ] = useState<boolean>(true);
+	const [ sigininclass, setSigninclass ] = useState<string>('');
+	const [ siginupclass, setSignupclass ] = useState<string>('');
 	const Navigate = useNavigate();
 	const fetchLogin = (e:React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -33,28 +36,62 @@ export default () => {
 			console.error(err);
 		};
 	};
+	useEffect(()=>{
+		if(sigininclass === '') {
+				setSigninclass('form signinForm');
+				setSignupclass('form signupForm');
+		} else {
+			if(isLoginPage) {
+				setSigninclass('form signinForm fadeIn');
+				setSignupclass('form signupForm fadeOut');
+				console.log(isLoginPage)
+			} else {
+				setSigninclass('form signinForm fadeOut');
+				setSignupclass('form signupForm fadeIn');
+			}
+		}
+	},[isLoginPage])
+	
 	return (
 		<div className='container'>
 			<div className='box'>
-				<form onSubmit={fetchLogin} className='form'>
-					<h2>Sign in</h2>
+				
+				{/* ログインフォーム */}
+				<form onSubmit={fetchLogin} className={sigininclass}>
+					<h2>ログイン</h2>
 					<div className='inputBox'>
 						<input type='text' onChange={(e)=>{setEmail(e.target.value)}} required />
-						<span>Username</span>
+						<span>メールアドレス</span>
 						<i></i>
 					</div>
 					<div className='inputBox'>
 						<input type='password' onChange={(e)=>{setPassword(e.target.value)}} required />
-						<span>password</span>
+						<span>パスワード</span>
 						<i></i>
 					</div>
 					<div className='links'>
-						<a href='javascript:void(0)'>Forgot Password</a>
-						<a href='javascript:void(0)'>sign-up</a>
+						<a href='javascript:void(0)'>パスワードをお忘れの方</a>
+						<a href='javascript:void(0)' onClick={()=>{setIsLoginPage(!isLoginPage)}}>新規登録</a>
 					</div>
-					<input type='submit' value="Login"/>
+					<input type='submit' value="ログイン"/>
+				</form>
+				
+				{/* 会員登録フォーム */}
+				<form onSubmit={fetchLogin} className={siginupclass}>
+					<h2>新規登録</h2>
+					<div className='inputBox'>
+						<input type='text' onChange={(e)=>{setEmail(e.target.value)}} required />
+						<span>メールアドレス</span>
+						<i></i>
+					</div>
+					<div className='links'>
+						<a href='javascript:void(0)'></a>
+						<a className='loginLink' href='javascript:void(0)' onClick={()=>{setIsLoginPage(!isLoginPage)}}>ログイン</a>
+					</div>
+					<input type='submit' value="確認メール"/>
 				</form>
 			</div>
 		</div>
 	)
 }
+
